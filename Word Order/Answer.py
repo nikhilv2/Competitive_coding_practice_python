6d49f112159e8dacc7d88ea34a0797c3849dc44f
Nikhil V Gopal <nikhilvenu2000@gmail.com>
2021-06-28 14:26:20 +0530

    Create Answer.py

---
+++ Word Order/Answer.py
@@ -0,0 +1,17 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import collections;
+
+N = int(input())
+d = collections.OrderedDict()
+
+for i in range(N):
+    word = input()
+    if word in d:
+        d[word] +=1
+    else:
+        d[word] = 1
+
+print(len(d));
+
+for k,v in d.items():
+    print(v,end = " ");
