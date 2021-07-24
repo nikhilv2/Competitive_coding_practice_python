b5846a101d3d9f714a0023e0792dad8757f25853
Nikhil V Gopal <nikhilvenu2000@gmail.com>
2021-07-24 19:31:11 +0530

    Create Answer.py

---
+++ Conditional Statements/Answer.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input().strip())
+    if N%2 != 0 or ((N > 5 and N < 21) and N%2 == 0):
+        print("Weird")
+    else:
+        print("Not Weird")
